36e869050099253348b899b128ee9cfa60916bcc
Jen Schumann <jenniferschumann1@gmail.com>
2018-02-08 16:41:22 -0700

    getting set up

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,12 @@
 // your code goes here ...
+// console.log('hhbuilder talking through js');
+
+//task 1: Validate data entry (age is required and > 0, relationship is required)
+const validateDataEntry = () => {
+  const n = document.forms["builderForm"]["age"].parseInt();
+  if(n <= 0) {
+    alert("Invalid age. Please complete form appropriately.");
+    return false;
+  }
+}
+validateDataEntry();
---
+++ server.js
@@ -0,0 +1,25 @@
+//dependencies
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//port
+app.listen(3000, function(){
+  console.log('hhbuilder is listening...');
+});
