253665561ecfc535ded108c438ce34a158b8b75b
Jen Schumann <jenniferschumann1@gmail.com>
2018-03-30 16:30:18 -0600

    minimalizing unnecessary code before refocusing on the challenge

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -2,11 +2,35 @@
 // console.log('hhbuilder talking through js');
 
 //task 1: Validate data entry (age is required and > 0, relationship is required)
+
+
 const validateDataEntry = () => {
-  const n = document.forms["builderForm"]["age"].parseInt();
-  if(n <= 0) {
-    alert("Invalid age. Please complete form appropriately.");
+
+  const n = document.getElementsByTagName("age");
+  const target = document.querySelector('#builder-div');
+  const div = document.createElement('div');
+  const x = parseInt(n);
+
+  if(x == "") {
+    // const div = document.createElement('div');
+
+    div.innerHTML = 'Error Age must be submitted.';
+    target.parentNode.insertBefore( div, target.nextSibling );
+    // alert('Error Age must be submitted.');
     return false;
-  }
-}
+  } else if
+    (isNaN(x) || x <= 0) {
+      // const div = document.createElement('div');
+      div.innerHTML = 'Invalid age. Please complete form appropriately.';
+      // target.parentNode.insertBefore( div, target.nextSibling );
+      // alert('Invalid age. Please complete form appropriately.');
+      return false;
+  } else {
+    // const div = document.createElement('div');
+      div.innerHTML = 'Input OK';
+      // target.parentNode.insertBefore( div, target.nextSibling );
+      };
+      // document.getElementById("demo").innerHTML = text;
+  };
+
 validateDataEntry();
